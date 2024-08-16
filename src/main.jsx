bfb8c97b69cc3350e2561bc5ff6ab8c2054c3118
Joao Lara <joaovdelara@gmail.com>
2024-08-16 11:20:01 -0300

    create root route

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,14 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom/client";
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Root from "./routes/root";
+
 import "./index.css";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <div>Hello world!</div>,
+    element: <Root />,
   },
 ]);
 
