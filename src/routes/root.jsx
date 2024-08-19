3f55959a6fcf06247517e5d6199bd875e7dec114
Joao Lara <joaovdelara@gmail.com>
2024-08-19 10:13:02 -0300

    fix form

--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -13,19 +13,34 @@ export async function loader() {
 
 export default function Root() {
   const { contacts } = useLoaderData();
-    return (
-      <>
-        <div id="sidebar">
-          <h1>React Router Contacts</h1>
-          <div>
+  return (
+    <>
+      <div id="sidebar">
+        <h1>React Router Contacts</h1>
+        <div>
+          <form id="search-form" role="search">
+            <input
+              id="q"
+              aria-label="Search contacts"
+              placeholder="Search"
+              type="search"
+              name="q"
+            />
+            <div
+              id="search-spinner"
+              aria-hidden
+              hidden={true}
+            />
+            <div
+              className="sr-only"
+              aria-live="polite"
+            ></div>
+          </form>
           <Form method="post">
             <button type="submit">New</button>
           </Form>
-            <form method="post">
-              <button type="submit">New</button>
-            </form>
-          </div>
-          <nav>
+        </div>
+        <nav>
           {contacts.length ? (
             <ul>
               {contacts.map((contact) => (
@@ -48,11 +63,11 @@ export default function Root() {
               <i>No contacts</i>
             </p>
           )}
-          </nav>
-        </div>
-        <div id="detail">
+        </nav>
+      </div>
+      <div id="detail">
         <Outlet />
-        </div>
-      </>
-    );
-  }
+      </div>
+    </>
+  );
+}
